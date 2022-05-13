1137a80145b5d011058556e6b2bb429b6a0664fd
Okoli Akachukwu <okoli555aka@gmail.com>
2022-05-13 11:14:57 +0100

    45% complete

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-// document.getElementById("part1_2_1_1").addEventListener("mouseover", () => {
-//     document.getElementById("arrow").fill = "#A1A1A1";
-// })
+const listOfImages = ["Images/desktop-image-hero-1.jpg", "Images/desktop-image-hero-2.jpg","Images/desktop-image-hero-3.jpg"];
+const listOfTitles = ["Discover innovative ways to decorate", "We are available all across the globe", "Manufactured with the best materials"];
+const listOfParagraphs = ["We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your style with our collection and make your property a reflection of you and what you know.", "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.", "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to eensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."]
+
+let currentSlide = 0;
+
+
+document.getElementById("left").addEventListener("click", () => {
+    if(currentSlide <= 2 && currentSlide >= 1){
+        currentSlide--;
+    }
+    document.getElementById("image").src = listOfImages[currentSlide];
+    document.getElementById("title").textContent = listOfTitles[currentSlide];
+    document.getElementById("paragraph").textContent = listOfParagraphs[currentSlide];
+})
+
+document.getElementById("right").addEventListener("click", () => {
+    if(currentSlide <= 1 && currentSlide >= 0){
+        currentSlide++;
+    }
+    document.getElementById("image").src = listOfImages[currentSlide];
+    document.getElementById("title").textContent = listOfTitles[currentSlide];
+    document.getElementById("paragraph").textContent = listOfParagraphs[currentSlide];
+})
+
