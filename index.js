c046ca9cb299a4ef0cff650ea9d39566ca6f4c9c
Okoli Akachukwu <okoli555aka@gmail.com>
2022-09-20 11:28:40 +0100

    fixed some issues concerning the responsivenenss

--- index.js
+++ index.js
@@ -4,24 +4,43 @@ const listOfParagraphs = ["We provide unmatched quality, comfort, and style for 
 
 let currentSlide = 0;
 
-
 document.getElementById("left").addEventListener("click", () => {
     if(currentSlide <= 2 && currentSlide >= 1){
         currentSlide--;
+        changeContentOfSlider(currentSlide);
     }
-    document.getElementById("image").src = listOfImages[currentSlide];
-    document.getElementById("title").textContent = listOfTitles[currentSlide];
-    document.getElementById("paragraph").textContent = listOfParagraphs[currentSlide];
 })
 
 document.getElementById("right").addEventListener("click", () => {
     if(currentSlide <= 1 && currentSlide >= 0){
         currentSlide++;
+        changeContentOfSlider(currentSlide);
     }
+})
+
+const changeContentOfSlider = (currentSlide) => {
     document.getElementById("image").src = listOfImages[currentSlide];
     document.getElementById("title").textContent = listOfTitles[currentSlide];
     document.getElementById("paragraph").textContent = listOfParagraphs[currentSlide];
-})
+}
+
+const changePositionOfSlider = () => {
+    let part1_2_2 = document.querySelector(".part1_2_2");
+    let temp = document.querySelector(".temp");
+
+    [...part1_2_2.children].forEach(element => {
+        temp.insertAdjacentElement('beforeEnd', element);
+      });
+}
+
+const reversechangePositionOfSlider = () => {
+    let part1_2_2 = document.querySelector(".part1_2_2");
+    let temp = document.querySelector(".temp");
+
+    [...temp.children].forEach(element => {
+        part1_2_2.insertAdjacentElement('beforeEnd', element);
+      });
+}
 
 document.getElementById("close").addEventListener("click", () => {
     document.getElementById("nav2").style.display = "none";
@@ -33,20 +52,54 @@ document.getElementById("hamburger").addEventListener("click", () => {
 
 document.getElementById("part1_2_1_1").addEventListener("mouseover", () => {
     document.getElementById("arrow").style.fill = "#A1A1A1";
-    document.getElementById("part1_2_1_1").style.color = "#A1A1A1"
+    document.querySelector("#part1_2_1_1 p").style.color = "#A1A1A1";
 })
 
 document.getElementById("part1_2_1_1").addEventListener("mouseout", () => {
     document.getElementById("arrow").style.fill = "#000";
-    document.getElementById("part1_2_1_1").style.color = "#000"
+    document.querySelector("#part1_2_1_1 p").style.color = "#000";
 })
 
 let jmediaquery = window.matchMedia( "(min-width: 499px)" );
 
-setInterval(()=> {
+const changeNavDisplay = setInterval(()=> {
     if (jmediaquery.matches) {
         document.getElementById("nav2").style.display = "none";
     }
 }, 1)
+
+let jMediaQuery = window.matchMedia( "(max-width: 749px)" );
+
+const loop = () => {
+    if (jMediaQuery.matches) {
+        changePositionOfSlider();
+        reverseloop();
+    } else if (!jMediaQuery.matches) {
+        const repeatChangePositionOfSlider = setInterval(() => {
+            loop();
+            clearInterval(repeatChangePositionOfSlider);
+        }, 1)
+    }
+}
+
+const reverseloop = () => {
+    if (!jMediaQuery.matches) {
+        reversechangePositionOfSlider();
+        loop();
+    } else if (jMediaQuery.matches) {
+        const repeatChangePositionOfSlider = setInterval(() => {
+            reverseloop();
+            clearInterval(repeatChangePositionOfSlider);
+        }, 1)
+    }
+}
+
+loop();
+
+
+
+
+
+
 
 
